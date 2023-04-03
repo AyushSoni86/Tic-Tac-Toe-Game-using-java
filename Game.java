bb5b453dfb81417280cc89c0b393d42bf4ab1b1a
AyushSoni86 <ayusoni86@gmail.com>
2023-04-03 18:16:25 +0530

    game created

---
+++ Game.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class Game {
+    public static void main(String[] args) {
+
+        char[][] board = new char[3][3];
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board.length; col++) {
+                board[row][col] = ' ';
+            }
+        }
+
+        char player = 'X';
+        boolean gameOver = false;
+        Scanner input = new Scanner(System.in);
+
+        while (!gameOver) {
+            printBoard(board);
+            System.out.println("Player " + player + " enter : ");
+            int row = input.nextInt();
+            int col = input.nextInt();
+
+            if (board[row][col] == ' ') {
+                board[row][col] = player;
+                gameOver = haveWon(board, player);
+                if (gameOver) {
+                    System.out.println("Player " + player + " has Won :)");
+                } else {
+                    player = (player == 'X') ? 'O' : 'X';
+                }
+            } else {
+                System.out.println("Invalid Move. Try Again!");
+            }
+        }
+    }
+
+    private static boolean haveWon(char[][] board, char player) {
+        // check the rows
+        for (int row = 0; row < board.length; row++) {
+            if (board[row][0] == player && board[row][1] == player && board[row][2] == player)
+                return true;
+        }
+
+        // check for columns
+        for (int col = 0; col < board.length; col++) {
+            if (board[0][col] == player && board[1][col] == player && board[2][col] == player)
+                return true;
+        }
+
+        // check for diagonals
+        if (board[0][0] == player && board[1][1] == player && board[2][2] == player)
+            return true;
+
+        if (board[0][2] == player && board[1][1] == player && board[2][0] == player)
+            return true;
+
+        return false;
+    }
+
+    private static void printBoard(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+        }
+        System.out.println("-----------");
+    }
+}
